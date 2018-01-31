b3490bb39ed2446e5d6d460d6f395b7880c9167e
Justin Tooley <jstooley823@gmail.com>
2018-01-31 03:10:11 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,8 +17,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    if session[:name] == nil
-    else
+    if session[:name] != nil
       session.delete :name
     end
   end
